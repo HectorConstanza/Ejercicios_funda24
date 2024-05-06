9595f300ac82421274fb2ae2ad091dc44f89a2c5
HectorConstanza <00012424@uca.edu.sv>
2024-05-06 12:19:34 -0600

    lazowhile

---
+++ lazowhile.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n = 0;
+    int i = 0;
+    cout << "ingrese un numero entero positivo";
+    cin >> n;
+
+    while (i<n)
+    {
+        cout << i << "\n";
+        i = i+1;
+    }
+
+    
+}
